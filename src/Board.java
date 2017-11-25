b7e407115e44dd21ab7d100ef9c8dda2f7bb7d5d
Ljungg <alvinljung@gmail.com>
2017-11-25 17:09:45 +0100

    Completed initiate()

--- src/Board.java
+++ src/Board.java
@@ -37,43 +37,57 @@ public class Board extends JPanel{
         for(int i=0;i<8;i++) {
             for(int j=0;j<8;j++) {
                 statusBoard[i][j] = null;
+                if(j==1) statusBoard[i][j] = new Pawn(Team.BLACK);
+                if(j==6) statusBoard[i][j] = new Pawn(Team.WHITE);
             }
         }
         statusBoard[0][0] = new Rook(Team.BLACK);
-        statusBoard[0][7] = new Rook(Team.BLACK);
-        statusBoard[0][1] = new Knight(Team.BLACK);
-        statusBoard[0][6] = new Knight(Team.BLACK);
-        statusBoard[0][2] = new Bishop(Team.BLACK);
-        statusBoard[0][5] = new Bishop(Team.BLACK);
-        statusBoard[0][3] = new King(Team.BLACK);
-        statusBoard[0][4] = new Queen(Team.BLACK);
-        statusBoard[7][0] = new Rook(Team.WHITE);
+        statusBoard[7][0] = new Rook(Team.BLACK);
+        statusBoard[1][0] = new Knight(Team.BLACK);
+        statusBoard[6][0] = new Knight(Team.BLACK);
+        statusBoard[2][0] = new Bishop(Team.BLACK);
+        statusBoard[5][0] = new Bishop(Team.BLACK);
+        statusBoard[3][0] = new Queen(Team.BLACK);
+        statusBoard[4][0] = new King(Team.BLACK);
+
+        statusBoard[0][7] = new Rook(Team.WHITE);
         statusBoard[7][7] = new Rook(Team.WHITE);
-        statusBoard[7][1] = new Knight(Team.WHITE);
-        statusBoard[7][6] = new Knight(Team.WHITE);
-        statusBoard[7][2] = new Bishop(Team.WHITE);
-        statusBoard[7][5] = new Bishop(Team.WHITE);
-        statusBoard[7][3] = new King(Team.WHITE);
-        statusBoard[7][4] = new Queen(Team.WHITE);
+        statusBoard[1][7] = new Knight(Team.WHITE);
+        statusBoard[6][7] = new Knight(Team.WHITE);
+        statusBoard[2][7] = new Bishop(Team.WHITE);
+        statusBoard[5][7] = new Bishop(Team.WHITE);
+        statusBoard[3][7] = new Queen(Team.WHITE);
+        statusBoard[4][7] = new King(Team.WHITE);
 
         for(int i = 0;i<8;i++){
-            instanceBoard.add(new ArrayList<Pieces>());
+            instanceBoard.add(new ArrayList<>());
             for(int j = 0;j<8;j++) {
                 instanceBoard.get(i).add(statusBoard[i][j]);
             }
         }
         System.out.println(instanceBoard);
+        System.out.println("Status board" + statusBoard);
     }
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-
-        //Graphics2D g2 = (Graphics2D) g;
         Image img1 = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Images/chessboard.png"));
-        Image img2 = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Images/Chess_qdt60.png"));
-        Image img3 = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Images/Chess_kdt60.png"));
         g.drawImage(img1, 0,0,this.getWidth(), this.getHeight(), this);
-        g.drawImage(img2, 0,0, this);
-        g.drawImage(img3, dimTile,dimTile, this);
+
+        Pieces piece;
+        int x;
+        int y;
+        for(int i = 0;i<8;i++){
+            for(int j = 0;j<8;j++){
+                piece = instanceBoard.get(i).get(j);
+                x = coordinateBoard.get(i);
+                y = coordinateBoard.get(j);
+                    if(piece != null){
+                    g.drawImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource(piece.getImage())), x, y, this);
+                }
+            }
+        }
+        //Graphics2D g2 = (Graphics2D) g;
+
     }
 }
